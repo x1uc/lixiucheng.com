c57a4a3ec95381afb546ba4026129c77e63aff3d
xiuc <ccuix@qq.com>
2026-08-01 13:02:10 +0800

    remove template author advertisements

--- dev-scripts-win/check-trailing-newline.ps1
+++ dev-scripts-win/check-trailing-newline.ps1
@@ -11,6 +11,10 @@ foreach ($relativePath in (git ls-files)) {
   }
 
   $path = Join-Path $repoRoot $relativePath
+  if (-not (Test-Path -LiteralPath $path -PathType Leaf)) {
+    continue
+  }
+
   $bytes = [System.IO.File]::ReadAllBytes($path)
 
   if ($bytes.Length -eq 0 -or $bytes -contains 0) {
